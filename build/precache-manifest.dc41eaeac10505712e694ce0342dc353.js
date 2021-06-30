5096b682d8a15cc72b26561351eaa4f368862302
phamnhat <15i1phamngocnhat@gmail.com>
2021-06-30 23:43:20 +0700

    update

---
+++ build/precache-manifest.dc41eaeac10505712e694ce0342dc353.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7872b6f0ecaa5c4caf79b6dd176d47fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2db6c9a2fc1aa779048",
+    "url": "/static/css/main.bed10205.chunk.css"
+  },
+  {
+    "revision": "59aa0cded94ad092b9ff",
+    "url": "/static/js/2.0b17fae8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0b17fae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2db6c9a2fc1aa779048",
+    "url": "/static/js/main.f886385c.chunk.js"
+  },
+  {
+    "revision": "f77588ce8f6a8453a55f",
+    "url": "/static/js/runtime-main.97ad1410.js"
+  },
+  {
+    "revision": "34b8a240cb97f45e9d4c4d6c38b63e4b",
+    "url": "/static/media/01.34b8a240.jpg"
+  },
+  {
+    "revision": "95f8d6d1f38da53671904b4fffb83555",
+    "url": "/static/media/01.95f8d6d1.jpg"
+  },
+  {
+    "revision": "bbafd47015929b9b8eee52f8c506256a",
+    "url": "/static/media/01.bbafd470.png"
+  },
+  {
+    "revision": "eaf474e308866ec37335ffc83fe6422f",
+    "url": "/static/media/01.eaf474e3.jpg"
+  },
+  {
+    "revision": "8efcf52569a73403861081a6907336bb",
+    "url": "/static/media/08.8efcf525.png"
+  }
+]);
